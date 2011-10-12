287381f023d10333cdb524539b99bdd84c671e98
Nathan Mentley <nathanmentley@gmail.com>
2011-10-12 11:19:30 -0400

    I've added cityname to lng,lat code inside the GooglePlaces datasource. It uses a local database I'm using and I've included it directly in the GooglePlaces class. Those are hackish and totally not acceptable for final code. I know, but I simply did it this way because we will probably only need this portion of code for the first program delivery Dr.Keller asked for. Past that we can use the code frank wrote in the PlacesOnRoute.java file.

--- org/placemap/tests/Google.java
+++ org/placemap/tests/Google.java
@@ -1,40 +1,27 @@
 // Google API test
 
-
 package org.placemap.tests;
 import org.placemap.datasource.*;
 
-
 import java.util.Scanner;
-//import java.util.Arrays;
-//import java.io.IOException;
 
 public class Google {
     public void mainLoop() {
-	double lat;
-	double longitude;
-	double radius;
-
 	GooglePlaces gp = new GooglePlaces("AIzaSyDkGX_KSZsT27e8EOROcFw8IE7P_5VAbiA");
-	
-
 	Scanner scan = new Scanner(System.in);
-
+	String city;
+	double radius;
 
 	System.out.printf(":: Test Google Places ::\n");
 	System.out.printf("Enter start point and end point.\n");
 	while (true) {
-	    
-	    System.out.printf("Enter latitude: ");
-	    lat = scan.nextDouble();
-	    System.out.printf("Enter longitude: ");
-	    longitude = scan.nextDouble();
+	    System.out.printf("Enter city: ");
+	    city = scan.next();
 	    System.out.printf("Enter radius: ");
 	    radius = scan.nextDouble();
 
 	    System.out.printf("Looking for places...\n");
-	    gp.getPlaces(lat,longitude,radius);
-
+	    gp.getPlaces(city,radius);
 	}
 
     }
